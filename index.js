7feb241562c7545e4840422c392b65daf2b4486a
Tharun Kumar <tharun97kumar@gmail.com>
2024-06-13 00:17:40 +0530

    Displaying flags data by fetching from databse

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+var quiz =[];
+const db = new pg.Client({
+  host:"localhost",
+  database:"mylove",
+  user:"postgres",
+  password:"postgres"
+
+});
+
+db.connect();
+
+db.query("select * from flags",(err,res)=>{
+if(err){
+  console.log("cannot fetch the data" + err.message);
+}
+else{
+ quiz = res.rows; 
+// console.log(quiz)
+}
+});
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", (req, res) => {
+  totalCorrect = 0;
+  nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
